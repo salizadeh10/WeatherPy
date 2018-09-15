24d92a440c130ad8f27c75b81036b3370748ca41
salizadeh10 <38050160+salizadeh10@users.noreply.github.com>
2018-09-14 18:51:40 -0700

    Took the comments from the starter code out.

--- WeatherPy.py
+++ WeatherPy.py
@@ -1,20 +1,4 @@
 
-# coding: utf-8
-
-# # WeatherPy
-# ----
-# 
-# ### Analysis
-# * As expected, the weather becomes significantly warmer as one approaches the equator (0 Deg. Latitude). More interestingly, however, is the fact that the southern hemisphere tends to be warmer this time of year than the northern hemisphere. This may be due to the tilt of the earth.
-# * There is no strong relationship between latitude and cloudiness. However, it is interesting to see that a strong band of cities sits at 0, 80, and 100% cloudiness.
-# * There is no strong relationship between latitude and wind speed. However, in northern hemispheres there is a flurry of cities with over 20 mph of wind.
-# 
-# ---
-# 
-# #### Note
-# * Instructions have been included for each segment. You do not have to follow them exactly, but they are included to help you think through the steps.
-
-# In[1]:
 
 
 # Dependencies and Setup
